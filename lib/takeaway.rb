a86e24dcf2e8ede697cc24c1e57f4b43141524e3
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-01-02 16:39:42 +0000

    Writes a test to select items and quantities from the menu. Uses instance_double for order, which 'has' an order_food method. Tests currently passing

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,12 +1,19 @@
 class Takeaway
-  attr_reader :menu 
+  attr_reader :menu, :items, :order
 
-  def initialize(menu)
+  def initialize(menu, order)
     @menu = menu
+    @order = order
   end 
 
   def read_menu
     menu.print_items
+  end 
+
+  def order_food(items)
+    items.each do |name, quantity|
+      order.add(name, quantity)
+    end
   end 
 
 end 
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,14 +2,19 @@ require 'takeaway'
 
 
  describe Takeaway do
-   subject(:takeaway) { described_class.new(menu) }
+   subject(:takeaway) { described_class.new(menu, order) }
    let(:items2) { "Pepperoni Pizza: 8.99" "Vegan Spectacular Pizza: 10.99" "Seafood Special Pizza: 10.99"}
    let(:menu) { instance_double("Menu", print_items: items2) }
+   let(:ordered_food) { {"Pepperoni Pizza": 2, "Vegan Spectacular Pizza": 2, "Seafood Special Pizza": 1} }
+   let(:order) { instance_double("Order", add: ordered_food) }
 
    it 'has a list of menu items and their respective prices' do
     expect(takeaway.read_menu).to eq items2
    end
   
+   it 'can select different quantities of dishes on the menu' do
+    expect(takeaway.order_food(ordered_food)).to eq ordered_food
+   end
    
   
 
